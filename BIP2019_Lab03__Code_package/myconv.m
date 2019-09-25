adc633ff91cd0fba5a3662dbae8649ca0ecc99ff
Balint Morasz <balint@morasz.hu>
2019-09-25 13:27:39 +0200

    lab2 done

--- BIP2019_Lab03__Code_package/myconv.m
+++ BIP2019_Lab03__Code_package/myconv.m
@@ -1,3 +1,11 @@
 function output_img = myconv(input_img, kernel)
-    
+    r = floor(size(kernel) ./ 2);
+    img = padarray(input_img, r);
+    kernel = rot90(rot90(kernel));
+    output_img = input_img .* 1;
+    for i=(r(1)+1):(size(input_img,1)+r(1))
+       for j=(r(2)+1):(size(input_img,2)+r(2))
+          output_img(i-r(1),j-r(2)) = sum( img(i-r(1):i+r(1), j-r(2):j+r(2)) .* kernel , 'all');
+       end
+    end
 end
