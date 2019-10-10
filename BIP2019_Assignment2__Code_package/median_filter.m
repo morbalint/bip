8b5e2cf601487b6e45a9afcb269d59b1cafcfcaa
Balint Morasz <balint@morasz.hu>
2019-10-10 11:46:05 +0200

    assignment 2 part 3 is done

--- BIP2019_Assignment2__Code_package/median_filter.m
+++ BIP2019_Assignment2__Code_package/median_filter.m
@@ -1,3 +1,15 @@
 function [filtered_img] = median_filter(in_img, r)
 
+[x_size, y_size] = size(in_img);
+filtered_img = zeros([x_size, y_size]);
+
+padded = padarray(in_img, [r,r], 'replicate');
+
+for i = r+1:x_size+r
+    for j = r+1:y_size+r
+        subImg = padded(i-r:i+r, j-r:j+r);
+        filtered_img(i,j) = median(subImg(:));
+    end
+end     
+        
 end
